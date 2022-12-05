b705af925c5bd5874d950a90efef1ee1cf8ac567
William <will_cable@hotmail.com>
2022-12-05 14:16:56 +0800

    Initial build plan created in JS file

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// 1) What can the user do?
+
+//When the user clicks on the "Start Quiz" button
+//Then they are taken to the  first question
+//Then the timer starts from 75 counting down
+
+//User picks an answer from one of 4 options
+//ANY answer will progress to the next question
+
+//When the next question is presented
+//Then the outcome of the previous question is displayed underneath the new list of options
+//The WRONG answer will deduct 10 seconds from the timer
+//The RIGHT answer will not have any effect on the timer
+
+//When all questions are answered, the time-left is presented as the 'final score'
+//--- When the timer reaches '0', the time left is presented as the 'final score'
+//Then the user can enter their initials in a textbox
+//Then the user can click the 'submit' button to save their result in the window memory.
+
+
+
+
+
